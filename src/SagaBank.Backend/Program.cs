5c314567eb00ecd62ebe1d9419a53d53aa554e9b
J. Zebedee <zebedee@code.gripe>
2023-04-12 10:30:01 -0500

    Add error handling

--- src/SagaBank.Backend/Program.cs
+++ src/SagaBank.Backend/Program.cs
@@ -1,5 +1,6 @@
 using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using SagaBank.Backend;
 using SagaBank.Kafka;
@@ -27,14 +28,27 @@ app.MapGet("/", () => $"Hello World! It's {DateTimeOffset.Now}");
 app.MapGet("/produce", ([FromServices] Producer producer) => producer.ProduceDummyData(kafkaSection["Topic"]));
 app.MapGet("/consume", ([FromServices] Consumer consumer) => consumer.ConsumeDummyData(kafkaSection["Topic"]));
 
-app.MapGet("/accounts/{id}", (int id, [FromServices] BankContext bank) => bank.Accounts.Single(a => a.AccountId == id));
-app.MapPost("/accounts",
-    (Account account, [FromServices] BankContext bank) =>
+app.MapGet("/accounts/{id}", (int id, [FromServices] BankContext bank)
+    => bank.Accounts.SingleOrDefault(a => a.AccountId == id) switch
     {
-        bank.Accounts.Add(account);
-        bank.SaveChanges();
+        Account account => Results.Ok(account),
+        null => Results.NotFound()
+    });
+app.MapPost("/accounts",
+    (Account account, [FromServices] BankContext bank, [FromServices] ILogger<Program> logger) =>
+    {
+        try
+        {
+            bank.Accounts.Add(account);
+            bank.SaveChanges();
 
-        return Results.Created($"/accounts/{account.AccountId}", account);
+            return Results.Created($"/accounts/{account.AccountId}", account);
+        }
+        catch (DbUpdateException outerEx) when (outerEx.InnerException is SqliteException ex and { SqliteErrorCode: SQLitePCL.raw.SQLITE_CONSTRAINT })
+        {
+            logger.LogError(outerEx, "New account conflict");
+            return Results.Conflict(account);
+        }
     });
 
 //app.MapGet("/accounts/{id}",
