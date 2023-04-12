22b1cb2957b180521250513e3dbcc63cc2829216
J. Zebedee <zebedee@code.gripe>
2023-04-12 10:02:07 -0500

    Add migration runner and account/balance endpoints

--- src/SagaBank.Backend/Program.cs
+++ src/SagaBank.Backend/Program.cs
@@ -8,7 +8,7 @@ using SagaBank.Kafka.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 var dbConnectionString = builder.Configuration.GetConnectionString(nameof(BankContext));
-builder.Services.AddDbContextPool<BankContext>(options => options.UseSqlite(dbConnectionString));
+builder.Services.AddDbContext<BankContext>(options => options.UseSqlite(dbConnectionString));
 
 var kafkaSection = builder.Configuration.GetSection("Kafka");
 builder.Services.AddKafkaProducer(configure => kafkaSection.GetSection(nameof(ProducerConfig)).Bind(configure));
@@ -16,8 +16,31 @@ builder.Services.AddKafkaConsumer(configure => kafkaSection.GetSection(nameof(Co
 
 var app = builder.Build();
 
+//FIXME: only for demo, don't use Migrate() in prod
+{
+    using var scope = app.Services.CreateScope();
+    var bank = scope.ServiceProvider.GetRequiredService<BankContext>();
+    bank.Database.Migrate();
+}
+
 app.MapGet("/", () => $"Hello World! It's {DateTimeOffset.Now}");
 app.MapGet("/produce", ([FromServices] Producer producer) => producer.ProduceDummyData(kafkaSection["Topic"]));
 app.MapGet("/consume", ([FromServices] Consumer consumer) => consumer.ConsumeDummyData(kafkaSection["Topic"]));
+
+app.MapGet("/accounts/{id}", (int id, [FromServices] BankContext bank) => bank.Accounts.Single(a => a.AccountId == id));
+app.MapPost("/accounts",
+    (Account account, [FromServices] BankContext bank) =>
+    {
+        bank.Accounts.Add(account);
+        bank.SaveChanges();
+
+        return Results.Created($"/accounts/{account.AccountId}", account);
+    });
+
+//app.MapGet("/accounts/{id}",
+//    (int id, [FromServices] BankContext bank) =>
+//    {
+//        return bank.Accounts.Single(a => a.AccountId == id);
+//    });
 
 app.Run();
