395ee95e4d7593a11ecf5e34f7e0385a277130de
J. Zebedee <zebedee@code.gripe>
2023-04-12 07:39:38 -0500

    Add Kafka usage in SagaBank.Backend

--- src/SagaBank.Backend/Program.cs
+++ src/SagaBank.Backend/Program.cs
@@ -1,6 +1,18 @@
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using SagaBank.Kafka;
+using SagaBank.Kafka.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var kafkaSection = builder.Configuration.GetSection("Kafka");
+builder.Services.AddKafkaProducer(configure => kafkaSection.GetSection(nameof(ProducerConfig)).Bind(configure));
+builder.Services.AddKafkaConsumer(configure => kafkaSection.GetSection(nameof(ConsumerConfig)).Bind(configure));
+
 var app = builder.Build();
 
 app.MapGet("/", () => $"Hello World! It's {DateTimeOffset.Now}");
+app.MapGet("/produce", ([FromServices] Producer producer) => producer.ProduceDummyData(kafkaSection["Topic"]));
+app.MapGet("/consume", ([FromServices] Consumer consumer) => consumer.ConsumeDummyData(kafkaSection["Topic"]));
 
 app.Run();
