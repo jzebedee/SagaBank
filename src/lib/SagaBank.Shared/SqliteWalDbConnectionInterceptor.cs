671d73829ff44d05b4ddfa6f82d942e913f9097b
J. Zebedee <zebedee@code.gripe>
2023-04-13 11:29:10 -0500

    Add SagaBank.Shared and move SqliteWalDbConnectionInterceptor to it

---
+++ src/lib/SagaBank.Shared/SqliteWalDbConnectionInterceptor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+
+namespace SagaBank.Shared;
+
+public class SqliteWalDbConnectionInterceptor : DbConnectionInterceptor
+{
+    private const string WalPragma = "PRAGMA journal_mode = 'wal';";
+
+    public override void ConnectionOpened(DbConnection connection, ConnectionEndEventData eventData)
+    {
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = WalPragma;
+        cmd.ExecuteNonQuery();
+    }
+
+    public override Task ConnectionOpenedAsync(DbConnection connection, ConnectionEndEventData eventData, CancellationToken cancellationToken = default)
+    {
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = WalPragma;
+        return cmd.ExecuteNonQueryAsync(cancellationToken);
+    }
+}
