3fb3f5eecfe6275f291b0f140cde28c66a90bc2c
J. Zebedee <zebedee@code.gripe>
2023-04-18 03:54:40 -0500

    Add SagaBank.Banking and Transaction Saga message stubs

---
+++ src/lib/SagaBank.Banking/TransactionMessages.cs
@@ -0,0 +1,26 @@
+﻿using MemoryPack;
+
+namespace SagaBank.Banking;
+
+[MemoryPackable]
+[MemoryPackUnion(0, typeof(TransactionStarting))]
+[MemoryPackUnion(1, typeof(TransactionStartFailed))]
+[MemoryPackUnion(2, typeof(TransactionUpdateBalanceAvailable))]
+[MemoryPackUnion(3, typeof(TransactionUpdateBalanceAvailableCompensation))]
+[MemoryPackUnion(4, typeof(TransactionUpdateBalanceAvailableFailed))]
+public partial interface ITransactionSaga { }
+
+[MemoryPackable]
+public partial record TransactionStarting(Ulid TransactionId, decimal Amount, int DebitAccountId, int CreditAccountId) : ITransactionSaga;
+
+[MemoryPackable]
+public partial record TransactionStartFailed(Ulid TransactionId, IDictionary<string, string[]> Errors) : ITransactionSaga;
+
+[MemoryPackable]
+public partial record TransactionUpdateBalanceAvailable(Ulid TransactionId, decimal Amount, int AccountId) : ITransactionSaga;
+
+[MemoryPackable]
+public partial record TransactionUpdateBalanceAvailableCompensation(Ulid TransactionId, decimal Amount, int AccountId) : ITransactionSaga;
+
+[MemoryPackable]
+public partial record TransactionUpdateBalanceAvailableFailed(Ulid TransactionId, IDictionary<string, string[]> Errors) : ITransactionSaga;
