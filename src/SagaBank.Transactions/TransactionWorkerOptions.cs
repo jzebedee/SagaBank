0e25dc93f6f7e8fae7716d34e840a1d05716b6b4
J. Zebedee <zebedee@code.gripe>
2023-04-21 15:06:48 -0500

    Add timeouts to TransactionWorkerOptions

--- src/SagaBank.Transactions/TransactionWorkerOptions.cs
+++ src/SagaBank.Transactions/TransactionWorkerOptions.cs
@@ -4,4 +4,8 @@ public class TransactionWorkerOptions
 {
     public string ConsumeTopic { get; set; }
     public string ProduceTopic { get; set; }
+    public TimeSpan TransactionTimeout { get; set; }
+    public TimeSpan ConsumeTimeout { get; set; }
+    public TimeSpan ThrottleTime { get; set; }
+    public TimeSpan CommitPeriod { get; set; }
 }
